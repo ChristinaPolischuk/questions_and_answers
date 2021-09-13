10e35785f5cd5be76f31acc8527f462ffab17ebd
frontEndDevel <front_end_dev@yahoo.com>
2021-09-13 12:25:25 +0300

    added form validation error fields

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,37 +1,41 @@
-const endpoint = '../../data/search-autocomplete.json';
+// const endpoint = '../../data/search-autocomplete.json';
+// const matchItems = [];
+// fetch(endpoint)
+// 	.then(raw => raw.json())
+// 	.then(data => matchItems.push(...data));
 
-const matchItems = [];
+// function findMatches(wordToMatch, matchItems) {
+// 	return matchItems.filter(matchItem => {
+// 		const regex = new RegExp(wordToMatch, "gi");
+// 		return matchItem.title.match(regex);
+// 	});
+// }
+// function numberWithCommas(x) {
+// 	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+// }
+// function displayMatches() {
+// 	const matchArray = findMatches(this.value, matchItems);
+// 	const html = matchArray.map(matchItem => {
+// 		const regex = new RegExp(this.value, 'gi');
+// 		const item = matchItem.title.replace(regex, `${this.value}`);
+// 		const url = matchItem.url;
+// 		return `<li class="match-item"><a href="${url}">${item}</a></li>`;
+// 	}).join('');
+// 	if (searchInput.value.length > 0) {
+// 		matchList.innerHTML = html;
+// 	} else {
+// 		matchList.innerHTML = '';
+// 	}
+// }
+// const searchInput = document.querySelector("#search");
+// const matchList = document.querySelector("#match-list");
+// searchInput.addEventListener("change", displayMatches);
+// searchInput.addEventListener("keyup", displayMatches);
 
-fetch(endpoint)
-	.then(raw => raw.json())
-	.then(data => matchItems.push(...data));
 
-function findMatches(wordToMatch, matchItems) {
-	return matchItems.filter(matchItem => {
-		const regex = new RegExp(wordToMatch, "gi");
-		return matchItem.match(regex);
-	});
+
+async function getResponse() {
+	return await fetch('../../data/search-autocomplete.json');
 }
 
-function numberWithCommas(x) {
-	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-}
-
-function displayMatches() {
-	const matchArray = findMatches(this.value, matchItems);
-	const html = matchArray.map(matchItem => {
-		const regex = new RegExp(this.value, 'gi');
-		const item = matchItem.replace(regex, `${this.value}`);
-		return `<li class="match-item">${item}</li>`;
-	}).join('');
-	if (searchInput.value.length > 0) {
-		matchList.innerHTML = html;
-	} else {
-		matchList.innerHTML = '';
-	}
-}
-
-const searchInput = document.querySelector("#search");
-const matchList = document.querySelector("#match-list");
-searchInput.addEventListener("change", displayMatches);
-searchInput.addEventListener("keyup", displayMatches);
+console.log(getResponse());
--- dist/static/js/main.js
+++ dist/static/js/main.js
@@ -1,1 +1,48 @@
-const endpoint="../../data/search-autocomplete.json",matchItems=[];function findMatches(n,t){return t.filter(t=>{var e=new RegExp(n,"gi");return t.match(e)})}function numberWithCommas(t){return t.toString().replace(/\B(?=(\d{3})+(?!\d))/g,",")}function displayMatches(){const t=findMatches(this.value,matchItems);var e=t.map(t=>{var e=new RegExp(this.value,"gi");return`<li class="match-item">${t.replace(e,`${this.value}`)}</li>`}).join("");0<searchInput.value.length?matchList.innerHTML=e:matchList.innerHTML=""}fetch(endpoint).then(t=>t.json()).then(t=>matchItems.push(...t));const searchInput=document.querySelector("#search"),matchList=document.querySelector("#match-list");searchInput.addEventListener("change",displayMatches),searchInput.addEventListener("keyup",displayMatches);
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
+// const endpoint = '../../data/search-autocomplete.json';
+// const matchItems = [];
+// fetch(endpoint)
+// 	.then(raw => raw.json())
+// 	.then(data => matchItems.push(...data));
+// function findMatches(wordToMatch, matchItems) {
+// 	return matchItems.filter(matchItem => {
+// 		const regex = new RegExp(wordToMatch, "gi");
+// 		return matchItem.title.match(regex);
+// 	});
+// }
+// function numberWithCommas(x) {
+// 	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+// }
+// function displayMatches() {
+// 	const matchArray = findMatches(this.value, matchItems);
+// 	const html = matchArray.map(matchItem => {
+// 		const regex = new RegExp(this.value, 'gi');
+// 		const item = matchItem.title.replace(regex, `${this.value}`);
+// 		const url = matchItem.url;
+// 		return `<li class="match-item"><a href="${url}">${item}</a></li>`;
+// 	}).join('');
+// 	if (searchInput.value.length > 0) {
+// 		matchList.innerHTML = html;
+// 	} else {
+// 		matchList.innerHTML = '';
+// 	}
+// }
+// const searchInput = document.querySelector("#search");
+// const matchList = document.querySelector("#match-list");
+// searchInput.addEventListener("change", displayMatches);
+// searchInput.addEventListener("keyup", displayMatches);
+function getResponse() {
+  return _getResponse.apply(this, arguments);
+}
+
+function _getResponse() {
+  _getResponse = _asyncToGenerator(function* () {
+    return yield fetch('../../data/search-autocomplete.json');
+  });
+  return _getResponse.apply(this, arguments);
+}
+
+console.log(getResponse());
